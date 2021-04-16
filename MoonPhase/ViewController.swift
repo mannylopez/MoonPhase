0f4f962491a4afcc1824ddc33a39ab10597bb2f1
mannylopez <manuel.est.lopez@gmail.com>
2021-04-15 21:56:41 -0700

    Initial Commit

---
+++ MoonPhase/ViewController.swift
@@ -0,0 +1,15 @@
+// Created by Manuel Lopez on 4/15/21.
+// Copyright © 2021 Airbnb Inc. All rights reserved.
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
